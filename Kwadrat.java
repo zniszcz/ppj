75d7572baaf31e1d82765c613cab5fa257e76dc2
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2017-12-09 11:44:25 +0100

    Add Zadanie7

--- Kwadrat.java
+++ Kwadrat.java
@@ -7,4 +7,9 @@ public class Kwadrat {
     System.out.println("Pole powierzchni tego kwadratu to: "+(this.bok*this.bok));
     System.out.println("Pole sześcianu zbudowanego z tego kwadratu to: "+(this.bok*this.bok*this.bok));
   }
+  public void showCylinder() {
+    float polePodstawy = (float)(this.bok * this.bok * Math.PI / 4);
+    System.out.println("Pole powierzchni podstawy tego walca to: "+polePodstawy);
+    System.out.println("Objetosc tego walca to: "+(polePodstawy * this.bok));
+  }
 }
---
+++ Zadanie7.java
@@ -0,0 +1,11 @@
+public class Zadanie7 {
+    public static void main (String[] args) {
+        Kwadrat kwadrat1 = new Kwadrat(5);
+        Kwadrat kwadrat2 = new Kwadrat(15);
+        Kwadrat kwadrat3 = new Kwadrat(3);
+
+        kwadrat1.showCylinder();
+        kwadrat2.showCylinder();
+        kwadrat3.showCylinder();
+    }
+}
